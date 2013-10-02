6696c362aab21c04992c32c1cd86f96e08e6e6d0
Michael Kohl <citizen428@gmail.com>
2013-10-03 00:30:55 +0700

    Renamed and simplified class

--- lib/pinboard_client.rb
+++ lib/pinboard_client.rb
@@ -1,31 +1,22 @@
 require 'httparty'
 
-class Pinboard
+class PinboardClient
   include HTTParty
   base_uri 'https://api.pinboard.in/v1/'
   headers "User-Agent" => "Unsavory"
-
-  API_METHODS = {
-    :all => '/posts/all',
-    :delete => '/posts/delete?url='}
 
   def initialize(user, password)
     @options = {:basic_auth => {:username => user, :password => password}}
   end
 
-  # get_all returns a nested hash, but we want an array of URLs
-  def get_all_urls
-    get_all['posts']['post'].inject([]) { |urls, post| urls << post['href'] }
+  def get_urls
+    response = self.class.get('/posts/all', @options)
+    if response.success?
+      response['posts']['post'].map { |post| post['href'] }
+    end
   end
 
   def delete(url)
-    self.class.get(API_METHODS[:delete]+url, @options)
-  end
-
-  private
-  def get_all
-    self.class.get(API_METHODS[:all], @options)
+    self.class.get("/posts/delete?url=#{url}", @options)
   end
 end
-
-
